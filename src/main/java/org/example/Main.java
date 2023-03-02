bc1b4e8211a2fe78ea5a0553a37175fdfae2f831
BakChanGyu <qmrma987@gmail.com>
2023-03-02 10:08:25 +0900

    6단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,6 +20,14 @@ public class Main {
         while (!comm.equals("종료")) {
             System.out.print("명령) ");
             comm = br.readLine();
+            int index = -1;
+            String rmComm = "";
+            index = comm.indexOf("?");
+            if (index != -1) {
+                rmComm = comm.substring(index + 1);
+                comm = comm.substring(0, index);
+            }
+
             if (comm.equals("등록")) {
                 System.out.print("명언 : ");
                 String quote = br.readLine();
@@ -35,6 +44,16 @@ public class Main {
                 quotesList.stream()
                         .sorted((e1, e2) -> e2.getId() - e1.getId())
                         .forEach(e -> System.out.println(e.toString()));
+            }
+
+            if (comm.equals("삭제")) {
+                int rmIndex = rmComm.indexOf("=");
+                int rmId = Integer.parseInt(rmComm.substring(rmIndex + 1));
+
+                quotesList = quotesList.stream()
+                        .filter(e -> e.getId() != rmId)
+                        .collect(Collectors.toList());
+                System.out.printf("%d번 명언이 삭제되었습니다.\n", rmId);
             }
         }
     }
